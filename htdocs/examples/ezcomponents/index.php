f13310c93cd898758b100d360d261280f6c851e2
fakko <fakko@7200729f-c539-0410-9c6d-c969e6bca738>
2007-11-28 22:54:46 +0000

    - Example graphs for eZ Components
    git-svn-id: svn://svn.code.sf.net/p/phpcallgraph/code/website@23 7200729f-c539-0410-9c6d-c969e6bca738

---
+++ htdocs/examples/ezcomponents/index.php
@@ -0,0 +1,11 @@
+<h1>PHPCallGraph Examples: eZ Components</h1>
+<p>
+The following call graphs of <a href="http://www.ezcomponents.org/">eZ Components</a>
+have been generated using <a href="http://phpcallgraph.sourceforge.net/">PHPCallGraph</a> v0.6.4
+from the eZ Component Subversion repository at 27.11.2007.
+Due to PHPCallGraph beeing still under development the graphs may not show all dependencies.
+</p>
+<?php
+require_once '../../../classes/ExampleViewer.php';
+new ExampleViewer;
+?>
